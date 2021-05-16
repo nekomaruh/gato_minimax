2f063348f7133a9c87f1cd320f8dcc97227f491b
Johan Esteban <johesteb@gmail.com>
2021-05-16 15:20:36 -0400

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,18 @@ import 'dart:io';
 
 import 'package:algoritmo_minimax/provider/game_provider.dart';
 import 'package:algoritmo_minimax/view/home.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:window_size/window_size.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
-    setWindowTitle('Taller Sistemas Inteligentes - Johan Ordenes - Dino Marín');
-    setWindowMinSize(const Size(500, 350));
+  if(!kIsWeb){
+    WidgetsFlutterBinding.ensureInitialized();
+    if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+      setWindowTitle('Taller Sistemas Inteligentes - Johan Ordenes - Dino Marín');
+      setWindowMinSize(const Size(500, 350));
+    }
   }
   runApp(MyApp());
 }
